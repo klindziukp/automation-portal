6dfbdb12e56af2d501174acf35a4d7b83bf3efc6
pavelklindziuk <pavel_klindziuk@epam.com>
2019-05-15 09:45:55 +0300

    AP-03 : add controller properties

--- src/test/java/com/dandelion/automationportal/script/service/BaseServiceTest.java
+++ src/test/java/com/dandelion/automationportal/script/service/BaseServiceTest.java
@@ -7,12 +7,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@DataMongoTest
 @ActiveProfiles("test")
 public class BaseServiceTest {
 
